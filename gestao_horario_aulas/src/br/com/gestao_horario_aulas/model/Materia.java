1942c4cba05941686cb0c36b64828b01d39053f8
Giovanni de lima conceição <giovannilima262@gmail.com>
2018-04-08 18:34:47 -0300

    Get e set da materia

--- gestao_horario_aulas/src/br/com/gestao_horario_aulas/model/Materia.java
+++ gestao_horario_aulas/src/br/com/gestao_horario_aulas/model/Materia.java
@@ -3,4 +3,20 @@ package br.com.gestao_horario_aulas.model;
 public class Materia {
 	private Curso curso;
 	private Integer semestre;
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
+	public Integer getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(Integer semestre) {
+		this.semestre = semestre;
+	}
 }
