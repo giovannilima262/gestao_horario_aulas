846c0d215c015e3bac8d4264a5ed6cf5d3c948c0
giovanni de lima <giovannilima262@gmail.com>
2018-06-17 15:01:59 -0300

    Inserir no controller de horario os campos para inserção correta dos horarios

--- gestao_horario_aulas/src/br/com/gestao_horario_aulas/controller/InserirHorarioController.java
+++ gestao_horario_aulas/src/br/com/gestao_horario_aulas/controller/InserirHorarioController.java
@@ -24,9 +24,7 @@ public class InserirHorarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private AulaDao aulaDao = new AulaDao();
-	private ProfessorDao professorDao = new ProfessorDao();
-	private DisciplinaDao disciplinaDao = new DisciplinaDao();
-	private SalaDao salaDao = new SalaDao();
+	// FIXME criar atributos dos DAOs para inserção
 
 	private List<Aula> aulas;
 
@@ -35,7 +33,7 @@ public class InserirHorarioController extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		/*
+		/* FIXME Editar
 		 * String id = req.getParameter("id"); try{
 		 * gradeDao.delete(Integer.parseInt(id)); }catch (SQLException e) {
 		 * req.setAttribute("mensagemErro", "Não é possivel deletar");
@@ -46,23 +44,26 @@ public class InserirHorarioController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		// FIXME Mudar para setar com base nos camposna tela
 		String hora = request.getParameter("hora");
 		String dia = request.getParameter("dia");
 		String idProfessor = request.getParameter("professor");
 		String idDisciplinaGrade = request.getParameter("disciplinagrade");
 		String idSala = request.getParameter("sala");
+		
+		//
+		
 		if (Util.isEmptyOrNull(hora) || Util.isEmptyOrNull(dia) || Util.isEmptyOrNull(idProfessor)
 				|| Util.isEmptyOrNull(idDisciplinaGrade) || Util.isEmptyOrNull(idSala)) {
-			request.setAttribute("mensagem", "Todos os dados são obrigatórios para inserir um professor");
-			request.getRequestDispatcher("./grade.jsp").forward(request, response);
+			request.setAttribute("mensagem", "Todos os dados são obrigatórios para inserir um horário");
+			request.getRequestDispatcher("./horario.jsp").forward(request, response);
 			return;
 		}
 
-		aulaDao.insert(new Aula(Integer.parseInt(hora), Integer.parseInt(dia),
-				professorDao.findById(Integer.parseInt(idProfessor)),
-				disciplinaDao.findByIdDisciplinaGrade(Integer.parseInt(idDisciplinaGrade)),
-				salaDao.findById(Integer.parseInt(idSala))));
-		response.sendRedirect("./listaGrades.jsp");
+		// FIXME inserir horario
+		
+		response.sendRedirect("./listaHorarios.jsp");
 	}
 
 	public List<Aula> getAulas() {
